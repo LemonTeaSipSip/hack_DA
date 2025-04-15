d5b010900dcd2ac8a863d317376559ae38dd4c0a
LemonTeaSipSip <130234042+LemonTeaSipSip@users.noreply.github.com>
2025-04-15 13:34:59 +0530

    Update

--- clickjacking-demo/attacker/server.js
+++ clickjacking-demo/attacker/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'sql212002', 
+  password: 'NewSecurePassword123!', 
   database: 'clickjacking_demo'
 });
 
